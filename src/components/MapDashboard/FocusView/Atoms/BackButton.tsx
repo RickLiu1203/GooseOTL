7264cabde940b31c2c8ed804d763f0986550c1fe
RickLiu1203 <rickliu1203@gmail.com>
2025-04-27 02:26:14 -0400

    add chevron rotation for back button

--- src/components/MapDashboard/FocusView/Atoms/BackButton.tsx
+++ src/components/MapDashboard/FocusView/Atoms/BackButton.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronUp } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 import React from 'react';
 
 interface Props {
@@ -7,25 +7,13 @@ interface Props {
 }
 
 function BackButton({ backClick, yPos }: Props) {
-
   return (
-    <>
-      { yPos <= 250 ?
-        <button
-        onClick={backClick}
-        className='fixed top-6 left-6 border-[1px] border-black bg-white text-black text-lg font-bold rounded-lg p-3 shadow-lg z-30'
-        >
-            <ChevronLeft size={20}/>
-        </button> :
-        <button
-          onClick={backClick}
-          className='fixed top-6 left-6 border-[1px] border-black bg-white text-black text-lg font-bold rounded-lg p-3 shadow-lg z-30'
-        >
-            <ChevronUp size={20} />
-        </button>
-      }
-    </>
-    
+    <button
+    onClick={backClick}
+    className='fixed top-6 left-6 border-[1px] border-black bg-white text-black text-lg font-bold rounded-lg p-3 shadow-lg z-30'
+    >
+        <ChevronLeft size={20} className={`${yPos >= 250 && "transform rotate-90"} duration-150`}/>
+    </button>
   );
 }
 
